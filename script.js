016c860177958a9f2602ed64f96b1a41adc00fdf
userNameAsString <99268849+userNameAsString@users.noreply.github.com>
2023-07-16 13:52:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const isMobile = typeof screen.orientation == 'undefined';
+const isMobile = /android.+mobile|ip(hone|[oa]d)/i.test(navigator.userAgent);
 function playAudio(number) {
      var audioFiles = document.getElementsByClassName("noiseButton");
     audioFiles[number].play();
